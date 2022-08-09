53c12490354d2af6a98747117fe4605e9071b433
noahbino <noahiarrobino@yahoo.com>
2022-08-09 15:00:48 -0500

    adding stuff

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-
-
-console.log('Client side javascript file is loaded!')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -11,14 +6,12 @@ const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
     const location = search.value
 
     messageOne.textContent = 'loading...'
     messageTwo.textContent = ''
-    
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             console.log(data.error)
